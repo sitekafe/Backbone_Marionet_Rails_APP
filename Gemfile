e71bc316338969627416b929102489c75b182c38
YOUR NAME <YOUR@EMAIL.com>
2016-02-13 18:24:16 +0200

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,98 @@
+#source 'https://rubygems.org'
+#ruby '2.2.3'
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+##gem 'rails', '4.2.0'
+#gem 'sass-rails', '~> 5.0'
+#gem 'uglifier', '>= 1.3.0'
+#gem 'coffee-rails', '~> 4.1.0'
+#gem 'jquery-rails'
+#gem 'jbuilder', '~> 2.0'
+#gem 'sdoc', '~> 0.4.0', group: :doc
+
+#group :development, :test do
+  #gem 'byebug'
+  #gem 'web-console', '~> 2.0'
+  #gem 'spring'
+  #gem 'thin'
+  #gem 'sqlite3'
+  #gem 'mysql'
+  	#gem 'mysql2', '~>0.3.10'
+  
+  	#gem 'guard-rspec', '0.5.5'
+#end
+
+#group :test do
+	#gem 'capybara', '1.1.2'
+	#gem 'rspec-rails', '2.9.0'
+	#gem 'rb-inotify', '0.8.8'
+	#gem 'libnotify', '0.5.9'
+	#gem 'spork-rails', '4.0.0'
+	#gem 'guard-spork', '1.5.0'
+  	#gem 'childprocess'
+#end
+
+# :production do
+ 
+  #gem 'rails_12factor'
+#end
+
+#gem 'bower-rails'
+#gem 'eco'
+#gem 'gon'
+#gem 'devise'
+#gem "js-routes"
+source 'https://rubygems.org'
+ruby '2.2.3'
+#ruby-gemset=railstutorial_rails_4_0
+
+gem 'rails', '4.2.0'
+gem 'bootstrap-sass'
+gem 'bcrypt-ruby'
+
+group :development, :test do
+  gem 'sqlite3'
+  gem 'rspec-rails'
+  gem 'guard-rspec'
+  gem 'spork-rails'
+  gem 'guard-spork'
+  gem 'childprocess'
+  gem 'annotate'
+end
+
+group :test do
+  gem 'selenium-webdriver'
+  gem 'capybara'
+
+  # OS X: раскомментируйте эти строки.
+  # gem 'growl', '1.0.3'
+
+  # Linux: раскомментируйте эти строки.
+  gem 'libnotify'
+
+  # Windows: раскомментируйте эти строки.
+  # gem 'rb-notifu', '0.0.4'
+  # gem 'win32console', '1.3.2'
+  # gem 'wdm', '0.1.0'
+end
+
+gem 'sass-rails'
+gem 'uglifier'
+gem 'coffee-rails'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder'
+gem 'rails-backbone'
+gem "js-routes"
+gem 'json'
+
+group :doc do
+  gem 'sdoc', '0.3.20', require: false
+end
+
+group :production do
+  gem 'mysql'
+  gem 'mysql2', '~>0.3.10'
+  #gem 'pg', '0.15.1'
+  gem 'rails_12factor'
+end
---
+++ app/assets/javascripts/script.js
@@ -0,0 +1,172 @@
+
+//APP
+window.App = new Backbone.Marionette.Application();
+
+App.addRegions({
+	main: '#main'
+});
+
+//Router
+App.Router = Backbone.Router.extend({
+      routes: {
+        ''                : 'first',
+        'users'           : 'first',
+        'users/new'       : 'newUser',
+        'users/:id/edit' : 'editUser',
+        'users/:id'      : 'showUser'
+      },
+
+      editUser: function (id){
+        var model = App.users.findWhere({id: Number(id)});
+        var editView = new App.EditView({model: model});
+        App.main.show(editView);
+      },
+
+      newUser: function (){
+        var newUser = new App.User();
+        var addForm = new App.AddView({model: newUser});
+        App.main.show(addForm);
+      },
+
+      first: function () {
+        var userListView = new App.UserListView({collection: App.users});
+        //regionView.render();
+        App.main.show(userListView);
+      },
+
+      showUser: function (id) {
+        var model = App.users.findWhere({id: Number(id)});
+        var showView = new App.ShowView({model: model});
+        App.main.show(showView);
+      }
+    });
+
+
+
+//Models&Collections
+
+ App.User = Backbone.Model.extend({
+      //idAttribute: "_id",
+      defaults : {
+        //id: id,
+        first_name  : '',
+        last_name   : ''
+      },
+      paramRoot: 'user'
+    });
+
+App.UserList = Backbone.Collection.extend({
+      model : App.User,
+      url: '/users'
+    });
+
+//Views
+//model
+App.UserView = Backbone.Marionette.ItemView.extend({
+      //tagName : 'div class="row"',
+      template: JST["templates/user"],
+      events : {
+        'click .glyphicon-remove' : 'delete'
+      },
+      delete: function(){
+        this.model.destroy();
+        App.users.remove(this.model);
+        return false;
+      }
+    });
+
+//collection
+
+App.UserListView = Backbone.Marionette.CompositeView.extend({
+      childView : App.UserView,
+      childViewContainer : '.container',
+      template: JST["templates/index"],
+      events: {
+        'keyup #filter': 'filter'
+      }
+      /*,
+      filter: function(e){
+        userListView.filter = function (child, index, collection) {
+          var query = $(e.currentTarget).val();
+          var nameRegex = new RegExp(query, "i");
+          return nameRegex.test(child.get('first_name'));
+        };
+        userListView.render();
+      }
+*/
+    });
+//add Form
+App.AddView = Backbone.Marionette.ItemView.extend({
+	//tagName: 'div class="container"',
+	template: JST["templates/new"],
+
+  events    : {
+        'click #btn_add' : 'addUser'
+      },
+
+  addUser : function (){
+        var first_name  = $('#first_name'),
+            last_name   = $('#last_name');
+            //uId = _.uniqueId();
+        App.users.create({
+          //id: uId,
+          first_name: first_name.val(),
+          last_name : last_name.val()
+        });
+        Backbone.history.navigate('#users', { trigger:true, replace: true });
+      }
+});
+
+//Show User
+
+App.ShowView = Backbone.Marionette.ItemView.extend({
+	//tagName: 'div class="container"',
+	template: JST["templates/show"],
+
+  serializeData : function(user) {
+        // Get the model attributes that need some formatting.
+        //var firstName = this.model.get('first_name');
+        //var lastName = this.model.get('last_name');
+
+        // Return an object to match the keys in your template.
+        return {
+            first_name  : this.model.get('first_name'),
+            last_name   : this.model.get('last_name')
+        };
+    }
+
+});
+
+//Edit User
+
+App.EditView = Backbone.Marionette.ItemView.extend({
+	//tagName: 'div class="container"',
+	template: JST["templates/edit"],
+  events: {
+          'click #edit_name' : 'editName'
+  },
+  editName: function() {
+    var first_name = $('#edit_first_name').val(),
+        last_name  = $('#edit_last_name').val();
+  this.model.set({
+    first_name: first_name,
+    last_name : last_name
+  });
+Backbone.history.navigate('#users', { trigger:true, replace: true });
+  }
+});
+
+//Collection
+
+//App.users = new App.UserList ();
+
+
+
+//var router = new App.Router();
+//App.on('start', function (){
+ //     Backbone.history.start();
+ //   });
+
+
+   App.start();
+
